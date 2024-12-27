cfea957ee8396e1f0f4f52556a1789fb52969035
Jaanuja Koneswaran <nithujaanu29@gmail.com>
2024-12-27 19:22:25 +0530

    Deleted some unwanted files

--- wowdash-file/src/App.js
+++ wowdash-file/src/App.js
@@ -1,86 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePageOne from "./pages/HomePageOne";
-import HomePageTwo from "./pages/HomePageTwo";
-import HomePageThree from "./pages/HomePageThree";
-import HomePageFour from "./pages/HomePageFour";
-import HomePageFive from "./pages/HomePageFive";
-import HomePageSix from "./pages/HomePageSix";
-import HomePageSeven from "./pages/HomePageSeven";
 import EmailPage from "./pages/EmailPage";
 import AddUserPage from "./pages/AddUserPage";
 import AlertPage from "./pages/AlertPage";
-import AssignRolePage from "./pages/AssignRolePage";
-import AvatarPage from "./pages/AvatarPage";
-import BadgesPage from "./pages/BadgesPage";
 import ButtonPage from "./pages/ButtonPage";
-import CalendarMainPage from "./pages/CalendarMainPage";
-import CardPage from "./pages/CardPage";
-import CarouselPage from "./pages/CarouselPage";
-import ChatEmptyPage from "./pages/ChatEmptyPage";
-import ChatMessagePage from "./pages/ChatMessagePage";
-import ChatProfilePage from "./pages/ChatProfilePage";
-import CodeGeneratorNewPage from "./pages/CodeGeneratorNewPage";
-import CodeGeneratorPage from "./pages/CodeGeneratorPage";
-import ColorsPage from "./pages/ColorsPage";
-import ColumnChartPage from "./pages/ColumnChartPage";
-import CompanyPage from "./pages/CompanyPage";
-import CurrenciesPage from "./pages/CurrenciesPage";
 import DropdownPage from "./pages/DropdownPage";
 import ErrorPage from "./pages/ErrorPage";
-import FaqPage from "./pages/FaqPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import FormLayoutPage from "./pages/FormLayoutPage";
 import FormValidationPage from "./pages/FormValidationPage";
 import FormPage from "./pages/FormPage";
-import GalleryPage from "./pages/GalleryPage";
-import ImageGeneratorPage from "./pages/ImageGeneratorPage";
-import ImageUploadPage from "./pages/ImageUploadPage";
-import InvoiceAddPage from "./pages/InvoiceAddPage";
-import InvoiceEditPage from "./pages/InvoiceEditPage";
-import InvoiceListPage from "./pages/InvoiceListPage";
-import InvoicePreviewPage from "./pages/InvoicePreviewPage";
-import KanbanPage from "./pages/KanbanPage";
-import LanguagePage from "./pages/LanguagePage";
-import LineChartPage from "./pages/LineChartPage";
-import ListPage from "./pages/ListPage";
-import MarketplaceDetailsPage from "./pages/MarketplaceDetailsPage";
-import MarketplacePage from "./pages/MarketplacePage";
 import NotificationAlertPage from "./pages/NotificationAlertPage";
 import NotificationPage from "./pages/NotificationPage";
 import PaginationPage from "./pages/PaginationPage";
-import PaymentGatewayPage from "./pages/PaymentGatewayPage";
-import PieChartPage from "./pages/PieChartPage";
-import PortfolioPage from "./pages/PortfolioPage";
-import PricingPage from "./pages/PricingPage";
-import ProgressPage from "./pages/ProgressPage";
-import RadioPage from "./pages/RadioPage";
-import RoleAccessPage from "./pages/RoleAccessPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
-import StarRatingPage from "./pages/StarRatingPage";
-import StarredPage from "./pages/StarredPage";
-import SwitchPage from "./pages/SwitchPage";
 import TableBasicPage from "./pages/TableBasicPage";
 import TableDataPage from "./pages/TableDataPage";
-import TabsPage from "./pages/TabsPage";
-import TagsPage from "./pages/TagsPage";
-import TermsConditionPage from "./pages/TermsConditionPage";
-import TextGeneratorPage from "./pages/TextGeneratorPage";
-import ThemePage from "./pages/ThemePage";
-import TooltipPage from "./pages/TooltipPage";
-import TypographyPage from "./pages/TypographyPage";
-import UsersGridPage from "./pages/UsersGridPage";
-import UsersListPage from "./pages/UsersListPage";
 import ViewDetailsPage from "./pages/ViewDetailsPage";
-import VideoGeneratorPage from "./pages/VideoGeneratorPage";
-import VideosPage from "./pages/VideosPage";
 import ViewProfilePage from "./pages/ViewProfilePage";
-import VoiceGeneratorPage from "./pages/VoiceGeneratorPage";
-import WalletPage from "./pages/WalletPage";
-import WidgetsPage from "./pages/WidgetsPage";
-import WizardPage from "./pages/WizardPage";
 import RouteScrollToTop from "./helper/RouteScrollToTop";
-import TextGeneratorNewPage from "./pages/TextGeneratorNewPage";
 import Group from "./components/groups/group";
 import AddGroup from "./components/groups/addGroup";
 import AddStudent from "./components/groups/addStudent";
@@ -88,6 +27,9 @@ import UploadFiles from "./components/groups/uploadFiles";
 import Files from "./components/files/files";
 import ViewBulkDetailsPage from "./pages/ViewBulkDetailsPage";
 import Login from "./components/login/login";
+import UsersGridPage from "./pages/UsersGridPage";
+import UsersListPage from "./pages/UsersListPage";
+
 
 
 function App() {
@@ -104,13 +46,7 @@ function App() {
         <Route exact path="/upload-files" element={<UploadFiles />} />
         <Route exact path="/files" element={<Files />} />
         <Route exact path="/alert" element={<AlertPage />} />
-        <Route exact path="/avatar" element={<AvatarPage />} />
         <Route exact path="/button" element={<ButtonPage />} />
-        <Route exact path="/card" element={<CardPage />} />
-        <Route exact path="/carousel" element={<CarouselPage />} />
-        <Route exact path="/chat-empty" element={<ChatEmptyPage />} />
-        <Route exact path="/chat-message" element={<ChatMessagePage />} />
-        <Route exact path="/chat-profile" element={<ChatProfilePage />} />
         <Route exact path="/dropdown" element={<DropdownPage />} />
         <Route exact path="/email" element={<EmailPage />} />
         <Route exact path="/forgot-password" element={<ForgotPasswordPage />} />
@@ -120,18 +56,16 @@ function App() {
         <Route exact path="/notification-alert" element={<NotificationAlertPage />} />
         <Route exact path="/notification" element={<NotificationPage />} />
         <Route exact path="/pagination" element={<PaginationPage />} />
-        <Route exact path="/radio" element={<RadioPage />} />
         <Route exact path="/sign-in" element={<SignInPage />} />
         <Route exact path="/sign-up" element={<SignUpPage />} />
         <Route exact path="/table-basic" element={<TableBasicPage />} />
         <Route exact path="/table-data" element={<TableDataPage />} />
         <Route exact path="/users-grid" element={<UsersGridPage />} />
+        <Route exact path="/users-list" element={<UsersListPage />} />
         <Route exact path="/group-list" element={<Group />} />
         <Route exact path="/view-details" element={<ViewDetailsPage />} />
         <Route exact path="/view-details/:id" element={<ViewDetailsPage />} />
         <Route exact path="/bulk-view-details" element={<ViewBulkDetailsPage />} />
-        <Route exact path="/video-generator" element={<VideoGeneratorPage />} />
-        <Route exact path="/videos" element={<VideosPage />} />
         <Route exact path="/view-profile" element={<ViewProfilePage />} />
         <Route exact path="*" element={<ErrorPage />} />
 
